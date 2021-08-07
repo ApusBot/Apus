00ed70a47cfbfdc7ee88fb892855c6bae69b981c
UraxUser <urax1243@gmail.com>
2021-08-07 15:26:19 +0200

    Added players module

--- bot/src/app.module.ts
+++ bot/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BotConfigModule } from './config/config.module';
 import { DiscordJsModule } from './discord-js/discord-js.module';
 import { HealthModule } from './health/health.module';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [BotConfigModule, DiscordJsModule, HealthModule],
+  imports: [BotConfigModule, DiscordJsModule, HealthModule, PlayersModule],
 })
 export class AppModule {}
